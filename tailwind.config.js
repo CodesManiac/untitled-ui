de8841ca4daf93e751fb169b73f24f83fcba245d
CodesManiac <joyajiboye2016@gmail.com>
2023-03-14 22:44:01 +0100

    setup

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    colors: {
+      blue: '#101828',
+      white: '#FFFFFF',
+
+      black: '#000000',
+
+      transparent: 'transparent',
+    },
+    fontFamily: {
+      Inter: ['Inter', 'sans-serif'],
+    },
+    extend: {},
+  },
+};
